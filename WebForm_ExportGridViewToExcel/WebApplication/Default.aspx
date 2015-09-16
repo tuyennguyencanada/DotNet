87d15be355e5a21b17bf1f6fb53318bd64f0a435
Tuyen Nguyen <tuyennguyencanada@gmail.com>
2015-09-16 13:27:38 -0600

    Export GridView Data to Excel - No Date Time Column in database

---
+++ WebForm_ExportGridViewToExcel/WebApplication/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form" runat="server">
+    <div>
+        <asp:GridView ID="GridView" runat="server">
+        </asp:GridView>
+    </div>
+        <asp:Button ID="btnExportExcel" runat="server" Text="Export to Excel" OnClick="btnExportExcel_Click" />
+    </form>
+</body>
+</html>
---
+++ WebForm_ExportGridViewToExcel/WebApplication/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                string constr = ConfigurationManager.ConnectionStrings["NorthwindMDF"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT * FROM [Alphabetical list of products]"))
+                    {
+                        using (SqlDataAdapter sda = new SqlDataAdapter())
+                        {
+                            cmd.Connection = con;
+                            sda.SelectCommand = cmd;
+                            using (DataTable dt = new DataTable())
+                            {
+                                sda.Fill(dt);
+                                GridView.DataSource = dt;
+                                GridView.DataBind();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public DataTable GetProducts()
+        {
+            using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["NorthwindMDF"].ConnectionString))
+            using (var cmd = new SqlCommand("SELECT * FROM [Alphabetical list of products]", conn))
+            using (var adapter = new SqlDataAdapter(cmd))
+            {
+                var products = new DataTable();
+                adapter.Fill(products);
+                return products;
+            }
+        }
+        protected void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            var products = GetProducts();
+            ExcelPackage excel = new ExcelPackage();
+            var workSheet = excel.Workbook.Worksheets.Add("Products");
+            var totalCols = products.Columns.Count;
+            var totalRows = products.Rows.Count;
+
+            for (var col = 1; col <= totalCols; col++)
+            {
+                workSheet.Cells[1, col].Value = products.Columns[col - 1].ColumnName;
+            }
+            for (var row = 1; row <= totalRows; row++)
+            {
+                for (var col = 0; col < totalCols; col++)
+                {
+                    workSheet.Cells[row + 1, col + 1].Value = products.Rows[row - 1][col];
+                }
+            }
+            using (var memoryStream = new MemoryStream())
+            {
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment;  filename=products.xlsx");
+                excel.SaveAs(memoryStream);
+                memoryStream.WriteTo(Response.OutputStream);
+                Response.Flush();
+                Response.End();
+            }
+            
+        }
+    }
+}
